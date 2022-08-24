d9f543a8308af2247a1ee14ae6963e8afabf7284
SSullivan <spencersullivan1990@gmail.com>
2022-08-24 12:38:02 -0500

    employee roster done

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,33 @@
 USE employee_db;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
--- insert employee names and info here
+    ('Lois', 'Griffin', 1, NULL),
+    ('Peter', 'Griffin', 2, 1),
+    ('Chris,' 'Griffin', 3, 1),
+    ('Meg', 'Griffin', 3, 1),
+    ('Stewie', 'Griffin', 3, 1),
+    ('Brian', 'Griffin', 4, 7),
+    ('Mort', 'Goldman', 8, NULL),
+    ('Glenn', 'Quagmire', 5, NULL),
+    ('Joe', 'Swanson', 6, 8),
+    ('Cleveland', 'Brown', 7, 8)
+
 
 INSERT INTO roles (title, salary, department_id) VALUES
--- insert roles info here
+    ('Head Coach', 90000, 1),
+    ('Assistant Coach', 75000, 1),
+    ('Player', 65000, 3),
+    ('Water Assistant', 50000, 5)
+    ('Human Resources', 55000, 3)
+    ('Security Guard', 65000, 4)
+    ('Marketer', 70000, 3)
+    ('Team Doctor', 90000, 5)
+
 
 INSERT INTO department (name) VALUES
--- insert roles here
+    ('Coaching'),
+    ('Team'),
+    ('Admin and Sales'),
+    ('Security'),
+    ('Team Staff')
+
