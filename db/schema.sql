3586afd6412e2243bc9e5a69dad544e7d4fa2111
SSullivan <spencersullivan1990@gmail.com>
2022-09-03 14:05:22 -0500

    trying new seeding method

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,3 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
-
-CREATE TABLE department_table(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE roles_table(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (department_id) REFERENCES department_table (id)
-);
-
-CREATE TABLE employee_table(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT DEFAULT NULL,
-    manager_id INT DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (role_id) REFERENCES employee_table (id),
-    FOREIGN KEY (manager_id) REFERENCES employee_table (id)
-);
