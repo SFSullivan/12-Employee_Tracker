9f3f390934f78ebac33613a7fe56a234981f0b17
SSullivan <spencersullivan1990@gmail.com>
2022-09-03 14:24:24 -0500

    connecting index with models

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const inquirer = require('inquirer');
 const mysql = require ('mysql2');
-const ctable = require('console.table')
-const db = require('./db/connection')
+const ctable = require('console.table');
+const db = require('./db/connection');
 
+const Department = require('./models/Department');
+const Employee = require('./models/Employee');
+const Role = require('./models/Role')
 
 //prompt questions and functions to follow
 
